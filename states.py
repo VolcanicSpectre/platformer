93e7ce533686088620ecb41ea97feb600f868b56
VolcanicSpectre <95970286+VolcanicSpectre@users.noreply.github.com>
2022-05-19 17:06:38 +0100

    more states

--- states.py
+++ states.py
@@ -1,15 +1,35 @@
 from abc import abstractclassmethod
+from enum import Enum, auto
+from sre_constants import JUMP
 
+class PlayerStates(Enum):
+    NULL = auto()
+    IDLE = auto()
+    RUN = auto()
+    DASH = auto()
+    JUMP = auto()
+    FALL = auto()
 
 class State:
+    def handle_inputs(keys): return PlayerStates.NULL
     
-    @abstractclassmethod
-    def handle_inputs(events): pass
-    
-    @abstractclassmethod
-    def update(dt): pass
+    def process(dt): return PlayerStates.NULL
+
+    def process_physics(): return PlayerStates.NULL
 
 
-class
+class MoveState(State):
+    def handle_inputs(keys):
+        if keys["up"]:
+            return PlayerStates.JUMP
+
+        return PlayerStates.NULL
+
+    def process_physics(dt):
+            
+
+
+    
+
 
 
