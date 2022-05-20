648107d3e5e130d0d3c2301b58ce1e3377077fcd
VolcanicSpectre <95970286+VolcanicSpectre@users.noreply.github.com>
2022-05-20 22:21:35 +0100

    more states + dash? + player re workl

--- player.py
+++ player.py
@@ -7,9 +7,15 @@ class Player:
     def __init__(self, x, y):
         self.state = PlayerStates.IDLE
         
+        self.image = pygame.Surface(self.size.copy())
+        self.image.fill("red")
+        self.rect = pygame.Rect(self.pos[0] , self.pos[1], self.size[0], self.size[1])
+        self.key_pressed = {"right":False , "left":False , "up":False}
+        
         self.x, self.y = x, y
         self.size = [8, 16]
         self.velocity = pygame.Vector2(0, 0)
+        
         self.MAXRUN = 4
         
         self.ACCELRUN = 0.07
@@ -19,7 +25,6 @@ class Player:
         self.STOPPOWER = 0.5
         self.ACCELPOWER = 0.5
         
-        
         self.JUMPHEIGHT = 50
         self.JUMPDISTANCE = 50
         self.INIT_JUMP_VELOCITY = ((2*self.jump_height*self.MAXRUN) / self.jump_distance) * -1
@@ -27,10 +32,7 @@ class Player:
         self.FINAL_GRAVITY = ((2*self.jump_height*self.MAXRUN**2) / (self.jump_distance ** 2)*1.2)
         
         self.air_timer = 0
-        self.image = pygame.Surface(self.size.copy())
-        self.image.fill("red")
-        self.rect = pygame.Rect(self.pos[0] , self.pos[1], self.size[0], self.size[1])
-        self.key_pressed = {"right":False , "left":False , "up":False}
+        
     
     def event_handler(self, event):   
         if event.type == pygame.KEYDOWN:
@@ -46,33 +48,21 @@ class Player:
                 case pygame.K_LEFT: self.key_pressed["left"] = False
                 case pygame.K_UP: self.key_pressed["up"] = False
                 case pygame.K_DOWN: self.key_pressed["down"] = False
-    
-    
-    def update_x(self, dt): 
-        if self.key_pressed["right"]:
-            self.x_move_input = 1
-        elif self.key_pressed["left"]:
-            self.x_move_input = -1
-        else:
-           self.x_move_input = 0
         
-        self.velocity.x = self.calculate_x_velocity(dt)
-        self.x += self.velocity.x
-        self.rect.x = self.x
+
+        new_state = self.state.handle_inputs()
+
+        if new_state: self.state = new_state
+
     
-    def update_y(self):
-        if self.key_pressed["up"] and self.air_frame < 4:
-            self.velocity.y = self.initial_jump_vel
-            self.air_frame = 1
-            self.key_pressed["up"] = False
-        
-        if self.velocity.y > 0:
-            self.velocity.y += self.final_gravity
-        else:
-            self.velocity.y += self.inital_gravity       
-        
-        self.y += self.velocity.y
-        self.rect.y = self.y
+    def update_x(self, dt):
+        new_state = self.state.process_x_movement(dt)
+        if new_state: self.state = new_state
+    
+    def update_y(self, dt):
+        new_state = self.state.process_y_movement(dt)
+        if new_state: self.state = new_state
+
     
     def get_direction(self):
         if self.key_pressed["right"]: return 1
--- states.py
+++ states.py
@@ -5,31 +5,44 @@ from functools import partial
 
 sign = partial(copysign, 1)
 class PlayerStates(Enum):
-    NULL = auto()
-    IDLE = auto()
-    RUN = auto()
-    DASH = auto()
-    JUMP = auto()
-    FALL = auto()
+    NULL = 0
+    IDLE = 1
+    RUN = 2
+    DASH = 3
+    JUMP = 4
+    FALL = 5
 
 class State:
     def __init__(self, player): self.player = player
     
     def handle_inputs(self, keys): return PlayerStates.NULL
     
-    def process(self, dt): return PlayerStates.NULL
+    def process_x_movement(self, dt): return PlayerStates.NULL
 
-    def process_physics(self, dt): return PlayerStates.NULL
+    def process_y_movement(self, dt): return PlayerStates.NULL
 
+
+
+class IdleState(State):
+    def handle_inputs(self, keys):
+        if self.player.get_direction(): return PlayerStates.RUN
+        if keys["up"]: return PlayerStates.JUMP
+        
+        return PlayerStates.NULL
+    
+    def process_y_movement(self, dt):
+        self.player.velocity.y += self.player.FINAL_GRAVITY * dt
+        if not self.player.grounded: return PlayerStates.FALL
+
+        return PlayerStates.NULL
 
 class RunState(State):
     def handle_inputs(keys):
-        if keys["up"]:
-            return PlayerStates.JUMP
+        if keys["up"]: return PlayerStates.JUMP
         return PlayerStates.NULL
         #TODO add dash
     
-    def process_physics(self, dt):
+    def process_x_movement(self, dt):
         dir = self.player.get_direction()
         
         if not dir: return PlayerStates.IDLE
@@ -37,13 +50,48 @@ class RunState(State):
         self.player.velocity.x = calculate_x_velocity(self.player, dt)
 
         return PlayerStates.NULL
+    
+    def process_y_movement(self, dt):
+        self.player.velocity.y += self.player.FINAL_GRAVITY * dt
+        if not self.player.grounded: return PlayerStates.FALL
 
+        return PlayerStates.NULL
 
 class DashState(RunState):
     DASHTIME = 0.4
-    def __enter__(self):
-        self.current_dash_time = DashState.DASHTIME
-        self.
+    def __iter__(self): 
+        yield DashState.DASHTIME
+
+class JumpState(State):    
+    def proceess_y_movement(self, dt):
+        self.player.velocity.y += self.player.INIT_GRAVITY * dt
+        
+        if self.player.grounded:
+            if self.player.key_pressed["right"] or self.player.key_pressed["left"]: return PlayerStates.RUN
+            else: return PlayerStates.IDLE
+
+    def process_x_movement(self, dt):
+        dir = self.player.get_direction()
+        self.player.velocity.x = calculate_x_velocity(self.player, dt)
+
+        return PlayerStates.NULL
+        
+class FallState(State):    
+    def proceess_y_movement(self, dt):
+        self.player.velocity.y += self.player.FINAL_GRAVITY * dt
+        
+        if self.player.grounded:
+            if self.player.key_pressed["right"] or self.player.key_pressed["left"]: return PlayerStates.RUN
+            else: return PlayerStates.IDLE
+
+    def process_x_movement(self, dt):
+        dir = self.player.get_direction()
+        self.player.velocity.x = calculate_x_velocity(self.player, dt)
+
+        return PlayerStates.NULL
+
+
+
 
 def calculate_x_velocity(entity, dt):
     dir = entity.get_direction()
@@ -59,29 +107,24 @@ def calculate_x_velocity(entity, dt):
         if not entity.grounded:
             a *= 0.6
     
-    if (u > target_speed and target_speed > 0.01) or (u < target_speed and target_speed < -0.01):
-        a = 0
+    if (u > target_speed and target_speed > 0.01) or (u < target_speed and target_speed < -0.01): a = 0
 
-    
-    if abs(target_speed) < 0.01:
-        vel_power = entity.STOPPOWER
-    elif abs(u) > 0 and sign(target_speed) != sign(u):
-        vel_power = entity.TURNPOWER
-    else:
-        vel_power = entity.ACCELPOWER
+    if abs(target_speed) < 0.01: vel_power = entity.STOPPOWER
+    elif abs(u) > 0 and sign(target_speed) != sign(u): vel_power = entity.TURNPOWER
+    else: vel_power = entity.ACCELPOWER
 
 
     v = (a*dt) ** vel_power
     
-    if dir > 0:
-        return min(u + v, target_speed)
-    if dir < 0:
-        return max(u + v*entity.x_move_input, target_speed)
+    if dir > 0: return min(u + v, target_speed)
+    if dir < 0: return max(u + v*entity.x_move_input, target_speed)
     else:
-        if sign(u) == -1:
-            return min(u - (v*-1), target_speed*entity.dir)
-        else:
-            return max(u - v, target_speed*dir)
+        if sign(u) == -1: return min(u - (v*-1), target_speed*entity.dir)
+        else: return max(u - v, target_speed*dir)
+
+
+
+
 
 
 
