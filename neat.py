196e167da55fa5e40a3a4e7ef239838124a413b3
VolcanicSpectre <95970286+VolcanicSpectre@users.noreply.github.com>
2022-11-15 17:22:23 +0000

    parity without NEAT

--- neat.py
+++
@@ -1,6 +0,0 @@
-class Neat:
-	def __init__(self):
-		self.current_generation_number = 0
-
-	def create_simulation(self):
-		self.current_generation = Generation()
--- node_types.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class NodeTypes(Enum):
-    INPUT = 3
-    HIDDEN = 2
-    OUTPUT = 1
