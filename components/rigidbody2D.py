0e97fa115ce997d66e33e7974e68643e0c9b407a
VolcanicSpectre <95970286+VolcanicSpectre@users.noreply.github.com>
2022-11-18 13:26:12 +0000

    working on rigid body 2d

--- components/rigidbody2D.py
+++ components/rigidbody2D.py
@@ -4,5 +4,9 @@ class RigidBody2D:
 	def __init__(self, x: float, y: float) -> None:
 		self.x = x
 		self.y = y
-
 		self.velocity = Vector2D(0, 0)
+
+	def add_force(self, force: Vector2D) -> None:
+
+
+
