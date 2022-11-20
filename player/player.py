1550ae24fa6df006f28af4d58c3e97391f3f6372
VolcanicSpectre <95970286+VolcanicSpectre@users.noreply.github.com>
2022-11-20 22:02:50 +0000

    implementing the player class

--- player/player.py
+++ player/player.py
@@ -1,6 +1,23 @@
+from components.input import Input
+from components.renderer import AnimatedRenderer
+from components.rigidbody2D import RigidBody2D
+from player.sub_states.player_idle_state import PlayerIdleState
+from player.sub_states.player_run_state import PlayerRunState
+from player.player_action_space import PlayerActionSpace
 from states.state_machine import StateMachine
 
 class Player:
 	def __init__(self):
-		self.state_machine = StateMachine(states.Idle)
+		self.idle_state = PlayerIdleState(self, "idle")
+		self.run_state = PlayerRunState(self, "run")
+		
+		self.renderer = AnimatedRenderer()
+		self.input = Input(PlayerActionSpace)
+		self.rb = RigidBody2D(5, 3)
+		
+	def start(self):
+		"""Creates the state machine for the player
+		"""
+		self.state_machine = StateMachine(self.idle_state)
+
 		
