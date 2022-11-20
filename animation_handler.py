b64911a65495d27194f185e6449cf97cadb3969f
VolcanicSpectre <95970286+VolcanicSpectre@users.noreply.github.com>
2022-11-20 13:35:39 +0000

    refactor

--- animation_handler.py
+++
@@ -1,6 +0,0 @@
-class AnimationHandler:
-    """
-    Provides and interface to handle animatiions for an entity
-    """
-    def __init__(self):
-        pass
--- states.py
+++
@@ -1,7 +0,0 @@
-from functools import partial
-from math import copysign
-
-
-
-sign = partial(copysign, 1)
-
