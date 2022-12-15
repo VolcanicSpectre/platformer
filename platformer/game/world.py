378f45d4e76aa3e133e5f735769a0d4ae8020a53
VolcanicSpectre <95970286+VolcanicSpectre@users.noreply.github.com>
2022-12-15 08:07:51 +0000

    working on new maploader

---
+++ platformer/game/world.py
@@ -0,0 +1,8 @@
+
+
+class World:
+	def __init__(self, world_num: int, current_level_num: int=0):
+		self.world_num = world_num
+		self.current_level_num = current_level_num
+
+		self.data = 
--- platformer/player/sub_states/player_in_air_state.py
+++ platformer/player/sub_states/player_in_air_state.py
@@ -1,1 +1,8 @@
-class 
+from platformer.calc.vector2d import Vector2D
+from player.player_action_space import PlayerActionSpace
+from platformer.states.player_state import PlayerState
+from player.player import Player
+
+class PlayerInAirState(PlayerState):
+	def __init__(self, player: Player, state_name: str):
+		super(PlayerInAirState, self).__init__(player, state_name)
